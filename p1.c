9d0d9711501499be8e00475a38d4b9582389e305
Anshupaija <paijaanshu223@gmail.com>
2024-04-04 15:46:34 +0530

    add

--- p1.c
+++ p1.c
@@ -6,7 +6,11 @@ int main()
     scanf("%d",&x);
     x=x*3;
     printf("Value of x after multiplying by 3:%d",x);
+    printf("Value of x after multiplying by 3:%d",x);
+    printf("Value of x after multiplying by 3:%d",x);
+    printf("Value of x after multiplying by 3:%d",x);
     x=x+10;
     printf("\nValue of X after adding 10:%d",x);
     return 0;
+    
 }
